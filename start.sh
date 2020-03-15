588fea5dc7ce502e71d6ace18c7bd7f1fe19e2b2
loic-roux <69loloro10@gmail.com>
2020-03-15 19:33:19 +0100

    add auto sync

--- start.sh
+++ start.sh
@@ -9,3 +9,10 @@ chown -Rf nginx.nginx /usr/share/nginx/html
 
 # Start supervisord and services
 /usr/local/bin/supervisord -n -c /etc/supervisord.conf
+
+rm -rf /usr/share/nginx/html/*
+git clone git@github.com:g4-dev/src-ecs.git /usr/share/nginx/html/ \
+cd /usr/share/nginx/html && git reset --hard origin/master
+touch update.php && chmod +x update.php
+echo "<?php `git pull origin master && composer -d ./www install -n` ?>" >> update.php
+php -r update.php
--- update.sh
+++
@@ -1,5 +0,0 @@
-rm -rf /usr/share/nginx/html/*
-git clone git@github.com:g4-dev/src-ecs.git /usr/share/nginx/html \
-cd /usr/share/nginx/html && git reset --hard origin/master
-cd www/
-composer install
